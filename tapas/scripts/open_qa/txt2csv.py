0b6bce74e786bbf9a0c914fe2cb6013a0af6a525
Chameleon Cloud User <cc@data-2.novalocal>
2021-07-20 14:25:20 -0500

    table2csv

--- tapas/scripts/open_qa/txt2csv.py
+++ tapas/scripts/open_qa/txt2csv.py
@@ -1,20 +1,22 @@
 import json
 import csv
+import os
 from tqdm import tqdm
 
 def read_table():
-    data_file = '/home/cc/data/nq/tables/tables.json'
-    with open(data_file) as f:
-        for line in f:
-            table = json.loads(line)
-            yield table
+    table_id_lst = [8, 19, 63, 102, 186, 209, 228, 289, 601, 962]
+    data_dir = '/home/cc/data/nq/interactions/dev_qas_tables/tables'
+    for table_id in table_id_lst:
+        table_name = 'table_%d' % table_id
+        data_file = os.path.join(data_dir, table_name, 'table.json')
+        with open(data_file) as f:
+            table_data = json.load(f)
+            yield (table_name, table_data)
 
 def main():
-    idx = 0
-    for table in tqdm(read_table()):
-        idx += 1
-        out_file = '/home/cc/data/nq/tables/output/table_%d.csv' % idx
-        with open(out_file, 'w', newline='') as f_o:
+    for table_name, table in tqdm(read_table()):
+        out_file = os.path.join('./output', '%s.csv' % table_name)
+        with open(out_file, 'w') as f_o:
             columns = table['columns']
             writer = csv.writer(f_o)
             col_names = [col_info['text'] for col_info in columns]
