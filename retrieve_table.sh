f11e07660aa73a5c625cdcf109c131b242ed5993
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-03-23 23:38:04 -0500

    fix dir

--- retrieve_table.sh
+++ retrieve_table.sh
@@ -1,4 +1,4 @@
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
   echo "Usage: ./retrieve_dev.sh <dataset> <checkpoint step> <mode>"
   exit
 fi
@@ -9,6 +9,6 @@ mode=$3
 model_dir=./models/${dataset}
 python tapas/scripts/eval_table_retriever.py \
  --prediction_files_local="${model_dir}/${mode}_${step}/predict_results_${step}.tsv" \
- --prediction_files_global="${model_dir}/tables/predict_results_${step}.tsv" \
+ --prediction_files_global="${model_dir}/tables_${step}/predict_results_${step}.tsv" \
  --retrieval_results_file_path="${model_dir}/${mode}_${step}/${mode}_knn.jsonl"
 
