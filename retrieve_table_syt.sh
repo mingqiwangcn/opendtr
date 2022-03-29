6fd65aab64b2b4d11f8733705b3abdbd36632ed1
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-03-29 11:56:56 -0500

    syt global predict

---
+++ retrieve_table_syt.sh
@@ -0,0 +1,14 @@
+if [ "$#" -ne 3 ]; then
+  echo "Usage: ./retrieve_dev.sh <dataset> <checkpoint step> <mode>"
+  exit
+fi
+
+dataset=$1
+step=$2
+mode=$3
+model_dir=./models/${dataset}_syt
+python tapas/scripts/eval_table_retriever.py \
+ --prediction_files_local="${model_dir}/${mode}_${step}/predict_results_${step}.tsv" \
+ --prediction_files_global="${model_dir}/tables_${step}/predict_results_${step}.tsv" \
+ --retrieval_results_file_path="${model_dir}/${mode}_${step}/${mode}_knn.jsonl"
+
