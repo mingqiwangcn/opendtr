1f43e630611bad3bee70e07dde10b7c3817c0184
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-04-16 21:50:09 -0500

    add mode dir arg

--- encode_tables.sh
+++ encode_tables.sh
@@ -1,20 +1,20 @@
-if [ "$#" -ne 2 ]; then
-  echo "Usage: ./encode_tables.sh <dataset> <checkpoint_step>"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: ./encode_tables.sh <dataset> <model dir> <checkpoint_step>"
   exit
 fi
 
 dataset=$1
-step=$2
+model_dir=$2
+step=$3
 data_dir=~/data/${dataset}
 max_seq_length=512
-model_dir=models/${dataset}
 retrieval_model_name=tapas_dual_encoder_proj_256_medium
 for mode in dev test tables; do
   echo "encoding ${mode}"
   python3 tapas/experiments/table_retriever_experiment.py \
      --do_predict \
      --model_dir="${model_dir}" \
-     --prediction_output_dir="${model_dir}/${mode}_${step}" \
+     --prediction_output_dir="${model_dir}/${dataset}_${mode}_${step}" \
      --input_file_predict="${data_dir}/tf_examples/${mode}.tfrecord" \
      --bert_config_file="${retrieval_model_name}/bert_config.json" \
      --init_from_single_encoder=false \
--- retrieve_table.sh
+++ retrieve_table.sh
@@ -1,14 +1,14 @@
-if [ "$#" -ne 3 ]; then
-  echo "Usage: ./retrieve_dev.sh <dataset> <checkpoint step> <mode>"
+if [ "$#" -ne 4 ]; then
+  echo "Usage: ./retrieve_dev.sh <dataset> <model dir> <step> <mode>"
   exit
 fi
 
 dataset=$1
-step=$2
-mode=$3
-model_dir=./models/${dataset}
+model_dir=$2
+step=$3
+mode=$4
 python tapas/scripts/eval_table_retriever.py \
- --prediction_files_local="${model_dir}/${mode}_${step}/predict_results_${step}.tsv" \
- --prediction_files_global="${model_dir}/tables_${step}/predict_results_${step}.tsv" \
- --retrieval_results_file_path="${model_dir}/${mode}_${step}/${mode}_knn.jsonl"
+ --prediction_files_local="${model_dir}/${dataset}_${mode}_${step}/predict_results_${step}.tsv" \
+ --prediction_files_global="${model_dir}/${dataset}_tables_${step}/predict_results_${step}.tsv" \
+ --retrieval_results_file_path="${model_dir}/${dataset}_${mode}_${step}/${mode}_knn.jsonl"
 
